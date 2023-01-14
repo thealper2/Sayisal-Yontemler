4aeca616cb711f55e81a99b9fe19a42775eadc10
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2023-01-14 22:07:21 +0300

    Update LagrangeInterpolation.m

--- LagrangeInterpolation.m
+++ LagrangeInterpolation.m
@@ -1,25 +1,23 @@
 clc;
 clear;
-close all;
 
 f = @(x)log(x);
-x = [1,4];
-myx = 2;
-sum = 0;
+x = [1, 4];
+x_ = 2;
+toplam = 0;
 n = 2;
 
 for i=1:1:2
-    product = f(x(i));
+    p = f(x(i));
     for j=1:1:2
-        if (i~=j)
-            product = product*(myx-x(j))/(x(i)-x(j));
+        if (i ~= j)
+            p = p * (x_ - x(j)) / (x(i) - x(j));
         end
     end
-    sum = sum + product;
+    toplam = toplam + p;
 end
 
-result = sum;
-disp("result: " + result);
-
-Error = abs((f(myx)-result)/f(myx))*100;
-disp("Error: " + Error);
+sonuc = toplam;
+disp("Sonuc: " + sonuc);
+Error = abs((f(x_)-sonuc) / f(x_)) * 100;
+disp("Hata: " + Error);
